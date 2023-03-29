c565b3fa0e7f5d8893e238c92ed071601b1421ce
unknown <pvuganeza@gmail.com>
2023-03-29 12:31:53 +0200

    fast fsat

--- fastAPI.py
+++ fastAPI.py
@@ -8,51 +8,37 @@ import os
 app = FastAPI()
 
 
+#Save the document locally
+def save_docx(filebytes : bytes, filename: str):
+    with open(f'documents/{filename}', 'wb') as docx:
+        docx.write(filebytes)
+    
+
 @app.get("/file", response_class=HTMLResponse)
-def upload_file_form(file: str):
+def upload_file_form():
     return """
-    <form method="post" action="/file">
+    <form method="post" action="/file" enctype="multipart/form-data">
     <!-- upload of a single file -->
     <p>
         <label>Add file (single): </label><br/>
-        <input type="file" name="example1"/>
+        <input type="file" name="file"/>
     </p>
 
     <p>
         <input type="submit"/>
     </p>
 </form>
-    """
+"""
 
-@app.post("/file1", response_class=HTMLResponse)
-def upload_file(file: str):
-    # with open(f"document.docx", "rb") as docx_file:
-    with open(file.filename, "rb") as docx_file:
-        result = mammoth.convert_to_html(docx_file)
-        html = result.value #the generated html
-        messages = result.messages #any messages, such as warnings during conversion
-        with open('output.html', 'w', encoding= 'utf-8') as htmlFile:
-            # htmlFile.write(result.value)
-            htmlFile.write(html)
-        with open('output.html', 'rb') as fh:
-          data = fh.read()
+@app.post("/file", response_class=HTMLResponse)
+async def upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
 
-    # Do here your stuff with the file
-    return Response(content=data, media_type="text/html")
+    #Save file
+    save_docx(contents, file.filename)
 
+    result = mammoth.convert_to_html(f'documents/{file.filename}')
+    messages = result.messages #any messages, such as warnings during conversion
 
-@app.post("/file")
-# async def root(file: UploadFile= File(...)):
-    # with open(f"document.docx", "rb") as docx_file:
-    #with open(file.filename, "rb") as docx_file:
-def file_resp():
-    path="C:/Users/Vugatri/Documents/fastapi"
-    file_path = os.path.join(path, 'output.html')
-    # if os.path.exists(file_path):
-    return FileResponse(file_path)
-    
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+    #return HTML Website
+    return f"{result.value}"
