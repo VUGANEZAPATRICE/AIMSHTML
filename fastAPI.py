f63025f8a798d5278955593f77045294bf77a308
Bateyjosue <josuebatey19@gmail.com>
2023-04-05 11:19:37 +0200

    Fix file format

--- fastAPI.py
+++ fastAPI.py
@@ -1,18 +1,17 @@
-from fastapi import FastAPI
-import mammoth 
-from fastapi import FastAPI, Response,File, UploadFile, Request
+'''
+    Server side for document translation
+'''
+import os
+from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse,FileResponse
-import os
+from fastapi.responses import HTMLResponse
 
 from bs4 import BeautifulSoup
 import aspose.words as aw
-import mammoth
 from googletrans import Translator
 from deep_translator import GoogleTranslator
-translator=Translator()
-from bs4 import BeautifulSoup
+translator = Translator()
 
 app = FastAPI()
 
@@ -20,15 +19,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-#Save the document locally
-def save_docx(filebytes : bytes, filename: str):
+# Save the document locally
+def save_docx(filebytes: bytes, filename: str):
     with open(f'assets/doc/{filename}', 'wb') as docx:
         docx.write(filebytes)
+
 
 def get_files(path):
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             yield file
+
 
 def docxToHtml(file):
     doc = aw.Document(f"assets/doc/{file}")
@@ -36,21 +37,23 @@ def docxToHtml(file):
     options.export_font_resources = True
     return doc.save(f'assets/html/{file[:-5]}.html', options)
 
+
 def translate_html(file):
     translator = GoogleTranslator(source='auto', target='rw')
     with open(f'assets/html/{file[:-5]}.html', 'r') as f:
         contents = f.read()
     soup = BeautifulSoup(contents, 'html.parser')
 
-    text_elements = [element for element in soup.find_all(string=True) if element.parent and element.parent.name not in ['script', 'style']]
+    text_elements = [element for element in soup.find_all(
+        string=True) if element.parent and element.parent.name not in ['script', 'style']]
     print(text_elements)
     for element in text_elements:
         # Extract the text from the element
         text = element.get_text(strip=True)
         # Skip the element if it's empty
-        if not text or text=="":
+        if not text or text == "":
             continue
-        splitted_text=""
+        splitted_text = ""
         for splitted in text.split("."):
             # Translate the text
             try:
@@ -59,26 +62,27 @@ def translate_html(file):
                 pass
             splitted_text = f'{splitted_text} {translated_text}'
         print(splitted_text)
-        
+
         element.string.replace_with(str(splitted_text))
     # Save modified HTML code to a file
     with open(f'assets/html/tran/{file[:-5]}.html', 'w') as f:
         f.write(str(soup))
 
+
 def convert_html_to_doc(path):
-    # import aspose.words as aw
     doc = aw.Document(f"assets/html/tran/{path[:-5]}.html")
     return doc.save(f"assets/doc/tran/{path[:-5]}.docx")
+
 
 @app.get("/", response_class=HTMLResponse)
 def upload_file_form(request: Request):
     files = get_files('assets/doc/tran')
 
-    return templates.TemplateResponse('index.html', context= {
-        'request' : request,
+    return templates.TemplateResponse('index.html', context={
+        'request': request,
         'files': files
-
     })
+
 
 @app.post("/", response_class=HTMLResponse)
 async def upload_file(request: Request, file: UploadFile = File(...)):
@@ -88,17 +92,16 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
     translate_html(file.filename)
     convert_html_to_doc(file.filename)
     files = get_files('assets/doc/tran')
-    return templates.TemplateResponse('index.html', context ={
-        "request":request,
+    return templates.TemplateResponse('index.html', context={
+        "request": request,
         'files': files
-        })
-
+    })
 
 
 @app.get('/translate')
 def translate(request: Request):
     files = get_files('templates/html')
-    return templates.TemplateResponse("html/output.html", context ={
-        "request":request,
+    return templates.TemplateResponse("html/output.html", context={
+        "request": request,
         'files': files,
-        })
+    })
