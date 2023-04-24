d8f55b464eac1dcce0a360d023a8b6d29b345547
Bateyjosue <josuebatey19@gmail.com>
2023-04-24 08:27:57 +0200

    fix path

--- fastAPI.py
+++ fastAPI.py
@@ -21,7 +21,7 @@ templates = Jinja2Templates(directory="templates")
 
 # Save the document locally
 def save_docx(filebytes: bytes, filename: str):
-    with open(f'/assets/doc/{filename}', 'wb') as docx:
+    with open(f'./assets/doc/{filename}', 'wb') as docx:
         docx.write(filebytes)
 
 
@@ -32,15 +32,15 @@ def get_files(path):
 
 
 def docxToHtml(file):
-    doc = aw.Document(f"/assets/doc/{file}")
+    doc = aw.Document(f"./assets/doc/{file}")
     options = aw.saving.HtmlSaveOptions()
     options.export_font_resources = True
-    return doc.save(f'/assets/html/{file[:-5]}.html', options)
+    return doc.save(f'./assets/html/{file[:-5]}.html', options)
 
 
 def translate_html(file):
     translator = GoogleTranslator(source='auto', target='rw')
-    with open(f'/assets/html/{file[:-5]}.html', 'r') as f:
+    with open(f'./assets/html/{file[:-5]}.html', 'r') as f:
         contents = f.read()
     soup = BeautifulSoup(contents, 'html.parser')
 
@@ -65,18 +65,18 @@ def translate_html(file):
 
         element.string.replace_with(str(splitted_text))
     # Save modified HTML code to a file
-    with open(f'/assets/html/tran/{file[:-5]}.html', 'w') as f:
+    with open(f'./assets/html/tran/{file[:-5]}.html', 'w') as f:
         f.write(str(soup))
 
 
 def convert_html_to_doc(path):
-    doc = aw.Document(f"/assets/html/tran/{path[:-5]}.html")
-    return doc.save(f"/assets/doc/tran/{path[:-5]}.docx")
+    doc = aw.Document(f"./assets/html/tran/{path[:-5]}.html")
+    return doc.save(f"./assets/doc/tran/{path[:-5]}.docx")
 
 
 @app.get("/", response_class=HTMLResponse)
 def upload_file_form(request: Request):
-    files = get_files('/assets/doc/tran')
+    files = get_files('./assets/doc/tran')
 
     return templates.TemplateResponse('index.html', context={
         'request': request,
@@ -91,7 +91,7 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
     docxToHtml(file.filename)
     translate_html(file.filename)
     convert_html_to_doc(file.filename)
-    files = get_files('/assets/doc/tran')
+    files = get_files('./assets/doc/tran')
     return templates.TemplateResponse('index.html', context={
         "request": request,
         'files': files,
